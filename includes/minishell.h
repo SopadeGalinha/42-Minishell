0de5c69b8c374ce3ae9ac7efaf63075a50ec88e9
Hellom-World <hellom-mendes@hotmail.com>
2023-09-23 00:50:04 +0100

    classifica key, value e full do **envp em array de array e salva na estrutura t_env_arr

--- includes/minishell.h
+++ includes/minishell.h
@@ -31,6 +31,13 @@
 
 /*__________________________________STRUCTS___________________________________*/
 
+typedef struct s_env_arr
+{
+	char			**key_array;
+	char			**value_array;
+	char			**full_array;
+}					t_env_arr;
+
 typedef struct s_env
 {
 	char			*key;
@@ -45,7 +52,7 @@ typedef struct s_env
 # define BOLD_RED		"\001\033[1;31m\002"
 # define BOLD_ORANGE	"\001\033[1;33m\002"
 # define BOLD_PURPLE	"\001\033[1;35m\002"
-# define BOLD_GREY		"\001\033[1;39m\002" 
+# define BOLD_GREY		"\001\033[1;39m\002"
 # define BOLD_GREEN		"\001\033[1;92m\002"
 # define BOLD_BLUE		"\001\033[1;94m\002"
 # define BOLD_CYAN		"\001\033[1;96m\002"
--- srcs/main.c
+++ srcs/main.c
@@ -19,14 +19,124 @@ void ft_handle_signals(void)
 	sigaction(SIGINT, &sig_actions, NULL);
 }
 
-t_env	init_env(char **envp)
+char	*ft_strdup_equal_value(const char *src)
 {
-	int	i = 0;
-	int	j = 0;
+	char	*cpy;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+
+	i = 0;
+	j = 0;
+	while (src[i] != '=')
+		i++;
+	k = ++i;
+	while (src[i++] != '\0')
+		j++;
+	cpy = (char *)malloc(sizeof(char) * (j + 1));
+	if (!cpy)
+		return (NULL);
+	j = 0;
+	while (src[k] != '\0')
+		cpy[j++] = src[k++];
+	cpy[j] = '\0';
+	return (cpy);
+}
+
+char	*ft_strdup_equal_key(const char *src)
+{
+	char	*cpy;
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '=')
+		i++;
+	cpy = (char *)malloc(sizeof(char) * i + 1);
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (*src != '=')
+		cpy[i++] = *src++;
+	cpy[i] = '\0';
+	return (cpy);
+}
+
+char	**get_key(char **envp)
+{
+	int i = 0;
+
+	while(envp[i])
+		i++;
+	char **key_array = malloc(sizeof(char *) * i + 1);
+	i = 0;
 	while (envp[i])
 	{
-		while ()
+		key_array[i] = ft_strdup_equal_key(envp[i]);
+		i++;
 	}
+	key_array[i] = NULL;
+	return (key_array);
+}
+
+char	**get_value(char **envp)
+{
+	int i = 0;
+
+	while(envp[i])
+		i++;
+	char **value_array = malloc(sizeof(char *) * i + 1);
+	i = 0;
+	while (envp[i])
+	{
+		value_array[i] = ft_strdup_equal_value(envp[i]);
+		i++;
+	}
+	value_array[i] = NULL;
+	return (value_array);
+}
+
+
+char	**get_full(char **envp)
+{
+	int i = 0;
+
+	while(envp[i])
+		i++;
+	char **full_array = malloc(sizeof(char *) * i + 1);
+	i = 0;
+	while (envp[i])
+	{
+		full_array[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	full_array[i] = NULL;
+	return (full_array);
+}
+
+t_env_arr	*init_env(char **envp)
+{
+	int i = 0;
+	char	**key_array;
+	char	**value_array;
+	char	**full_array;
+	t_env_arr	*env;
+
+	env = (t_env_arr *)malloc(sizeof(t_env_arr));
+	env->key_array = get_key(envp);
+	env->value_array = get_value(envp);
+	env->full_array = get_full(envp);
+
+	while(env->key_array[i])
+		ft_printf_fd(1, "%s\n", env->key_array[i++]);
+	i = 0;
+	printf("--------------------------\n");
+	while(env->value_array[i])
+		ft_printf_fd(1, "%s\n", env->value_array[i++]);
+	i = 0;
+	printf("--------------------------\n");
+	while(env->full_array[i])
+		ft_printf_fd(1, "%s\n", env->full_array[i++]);
+	return (env);
 }
 
 int main(int ac, char **av, char **envp)
@@ -34,7 +144,7 @@ int main(int ac, char **av, char **envp)
 	char *input;
 	int		i = 0;
 	int		j = 0;
-	t_env	*env;
+	t_env_arr	*env_arr;
 
 	input = NULL;
 	if (ac != 1 || !av)
@@ -43,7 +153,7 @@ int main(int ac, char **av, char **envp)
 	using_history();
 	ft_handle_signals();
 
-	env = init_env(envp);
+	env_arr = init_env(envp);
 	while (1)
 	{
 		free(input);
