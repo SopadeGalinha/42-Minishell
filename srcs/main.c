77cf5d7ce735334c98188cd52541efb8e8553c5c
Hellom Oliveira Mendes <heolivei@c1r13s3.42porto.com>
2023-10-06 21:32:29 +0100

    Faz os comandos do export: adicionar novo noh e atualizar caso a key ja exista tanto para a lista env quanto para a lista exp

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,11 @@
 #include "../includes/minishell.h"
 
-void	free_tokens(t_token **tokens)
+void	free_tokens(t_shell *shell)
 {
 	t_token	*current;
 	t_token	*tmp;
 
-	current = *tokens;
+	current = shell->tokens;
 	while (current != NULL)
 	{
 		tmp = current->next;
@@ -13,7 +13,7 @@ void	free_tokens(t_token **tokens)
 		free(current);
 		current = tmp;
 	}
-	*tokens = NULL;
+	shell->tokens = NULL;
 }
 
 void	init_shell(t_shell *shell, char **env)
@@ -38,6 +38,101 @@ bool	get_input(t_shell *shell)
 	return (true);
 }
 
+int create_add_node_to_back(t_env **head, char *line)
+{
+    t_env *new = (t_env *)malloc(sizeof(t_env));
+    t_env *temp = *head;
+    if (!new) {
+        ft_printf_fd(2, "Failed to allocate memory for t_env\n");
+        return (0);
+    }
+    new->key = ft_strdup_equal_key(line);
+    new->value = ft_strdup_equal_value(line);
+    new->line = ft_strdup(line);
+    new->next = NULL;
+
+    if (!*head) {
+        *head = new;  // Se a lista estiver vazia, o novo nó é o único nó na lista.
+        return (1);
+    }
+
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = new;  // Adiciona o novo nó após o último nó existente.
+    return (1);
+}
+
+int create_find_add_insert_node(t_env **head, char *line)
+{
+    t_env *new = (t_env *)malloc(sizeof(t_env));
+    if (!new) {
+        ft_printf_fd(2, "Failed to allocate memory for t_env\n");
+        return (0);
+	}
+    new->key = ft_strdup_equal_key(line);
+    new->value = ft_strdup_equal_value(line);
+    new->line = ft_strdup(line);
+    new->next = NULL;
+
+	
+    if (!*head) {
+        *head = new;  // Se a lista estiver vazia, o novo nó é o único nó na lista.
+        return (1);
+    }
+   	insert_sorted(head, new);
+    return (1);
+}
+
+
+void	update_exp(t_shell *shell, char *line)
+{
+	create_find_add_insert_node(&shell->exp, line);
+}
+int	find_node(t_env *lst, char *key)
+{
+	t_env *current = lst;
+
+    while (current != NULL) 
+	{
+        if (ft_strncmp(current->key, key, ft_strlen(current->key)) == 0) 
+            return (1);
+        current = current->next;
+    }
+    return (0);
+}
+void	update_node(t_env *lst, char *key, char *line)
+{
+	t_env *current = lst;
+
+    while (current != NULL) {
+        if (ft_strncmp(current->key, key, ft_strlen(current->key)) == 0)
+		{
+            // Atualiza o valor do nó correspondente
+			current->key = ft_strdup_equal_key(line);
+    		current->value = ft_strdup_equal_value(line);
+    		current->line = ft_strdup(line);
+            return;
+        }
+        current = current->next;
+    }
+}
+
+void	update_lists(t_shell *shell, char *line)
+{
+	char	*key = ft_strdup_equal_key(line);
+	if (find_node(shell->env, key))
+		update_node(shell->env, key, line);
+	else
+		create_add_node_to_back(&shell->env, line);
+	
+	if (find_node(shell->exp, key))
+		update_node(shell->exp, key, line);
+	else
+		create_find_add_insert_node(&shell->exp, line);
+
+	
+}
 int main(int ac, char **av, char **envp)
 {
 	t_shell	shell;
@@ -59,11 +154,22 @@ int main(int ac, char **av, char **envp)
 			print_list(shell.env, 1);
 		if ((ft_strncmp(shell.input, "export", ft_strlen(shell.input)) == 0))
 			print_list(shell.exp, 0);
-		/*if (shell.tokens->data == "export")
-			printf("%s\n", shell.tokens->next->data);*/
+		if ((ft_strncmp(shell.tokens->data, "export", ft_strlen(shell.tokens->data)) == 0) && shell.tokens->next)
+		{
+			printf("%s\n", shell.tokens->next->data);
+			if (ft_strchr(shell.tokens->next->data, '='))
+			{
+				update_lists(&shell, shell.tokens->next->data);
+			}
+			if (!ft_strchr(shell.tokens->next->data, '='))
+			{	
+				update_exp(&shell, shell.tokens->next->data);
+			}
+		}
 		add_history(shell.input);
-		free_tokens(&shell.tokens);
+		free_tokens(&shell);
 	}
+
 	if (shell.input)
 		free(shell.input);
 	if (shell.path_env)
