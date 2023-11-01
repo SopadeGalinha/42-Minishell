8707d07fa9ad0e4be5867f5c4dff5a97d2624e44
Hellom-World <hellom-mendes@hotmail.com>
2023-11-01 16:23:41 +0000

    Retirar parte do excute para merge

--- srcs/execute.c
+++ srcs/execute.c
@@ -3,14 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhogonca <jhogonca@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: heolivei <heolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 18:18:41 by jhogonca          #+#    #+#             */
-/*   Updated: 2023/11/01 14:12:27 by jhogonca         ###   ########.fr       */
+/*   Updated: 2023/11/01 16:22:15 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	token_checker(char *str)
+{
+	int	i = 0;
+
+	if (ft_isdigit(str[0]))
+	{
+		printf("nao pode colocar numero no inicio na key vacilao\n");
+		return (-1);
+	}
+	else if (str[0] == '_' && str[1])
+	{
+		if(!ft_isalnum(str[1]) && str[1] != '_')
+		{
+			printf("Depois do UNDERLINE so pode numero, letra ou outro UNDELINE\n");
+			return (-1);
+		}
+		else
+			return (1);
+	}
+	else if (str[0] == '_' && (str[1] == '=' || str[1] == '\0'))
+	{
+		printf("Apenas UNDERLINE o codigo nao faz nada\n");
+		return (-2);
+	}
+	return (1);
+}
+
+void	ft_export(t_shell *shell)
+{
+	int	i = 1;
+
+	if (shell->pipes->cmds[i])
+	{
+		while (shell->pipes->cmds[i])
+		{
+			if(token_checker(shell->pipes->cmds[i]) > 0)
+			{
+				if (ft_strchr(shell->pipes->cmds[i], '='))
+					update_lists(shell, shell->pipes->cmds[i], 1);
+				else if (!ft_strchr(shell->pipes->cmds[i], '='))
+					update_lists(shell, shell->pipes->cmds[i], 0);
+			}
+			i++;
+		}
+	}
+    else
+        print_list(shell, 0);
+}
 
 void	execute(t_shell *shell)
 {
@@ -23,26 +72,6 @@ void	execute(t_shell *shell)
 	builtin[2] = "echo";
 	function = -1;
 	pipes = shell->pipes;
-	while (++function < 3)
-	{	
-		if (ft_strncmp(pipes->cmds[0], builtin[function], \
-		ft_strlen(builtin[function])) == 0
-			&& ft_strlen(pipes->cmds[0]) == ft_strlen(builtin[function]))
-			break ;
-		if (ft_strncmp(pipes->cmds[0], "exit", 4) == 0 \
-			&& ft_strlen(pipes->cmds[0]) == 4)
-			free_struct(shell, 1);
-		if (ft_strncmp(pipes->cmds[0], "unset", 5) == 0 \
-			&& ft_strlen(pipes->cmds[0]) == 5)
-			ft_unset(shell, pipes->cmds[1]);
-		if (ft_strncmp(pipes->cmds[0], "export", 6) == 0 \
-			&& ft_strlen(pipes->cmds[0]) == 6)
-			print_list(shell, 0);
-		if (ft_strncmp(pipes->cmds[0], "env", 3) == 0 \
-		&& ft_strlen(pipes->cmds[0]) == 3)
-			print_list(shell, 1);
-	}
-	if (function < 3)
-		shell->builtin[function](shell);
+
 }
 
