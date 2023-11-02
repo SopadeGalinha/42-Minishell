cd1be4ea583d27400a640a4dd9dd6303a2fcc1e1
Jhonata Goncalves Pereira <jhogonca@c1r15s9.42porto.com>
2023-11-02 20:30:06 +0000

    access in excute.c

--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -12,33 +12,7 @@
 
 #include "../includes/minishell.h"
 
-bool	ft_access(t_shell *shell)
-{
-	t_pipes	*tmp;
-	char	**array;
-	int		i;
-	char	*path;
 
-	i = -1;
-	tmp = shell->pipes;
-	path = get_env_value(shell->env, "PATH");
-	array = ft_split(path, ':');
-	while (array[++i])
-	{
-		free(path);
-		path = ft_strjoin(array[i], "/");
-		path = ft_strjoin(path, tmp->cmds[0]);
-		ft_printf_fd(1, "path: %s\n", path);
-		 if (access(path, F_OK) == 0)
-		{
-			tmp->path = ft_strdup(path);
-			free(path);
-			break ;
-		}
-	}
-	execve(tmp->path, tmp->cmds, NULL);
-	return (true);
-}
 
 bool	parse_input(t_shell *shell)
 {
@@ -50,8 +24,6 @@ bool	parse_input(t_shell *shell)
 		return (false);
 	if (shell->error == true)
 		printf("Error só n sei qual\n");
-	if (ft_access(shell) == false)
-		return (false);
 	print_pipes(shell->pipes);
 	return (true);
 }
