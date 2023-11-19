88a9ac671817dac7aa1d42e6831b9e89830a9308
root <root@Marla.>
2023-11-19 22:21:58 +0000

    auto commit

--- srcs/execute.c
+++ srcs/execute.c
@@ -5,39 +5,39 @@
 
 int ft_envsize(t_env *lst)
 {
-    int i = 0;
-    while (lst)
-    {
-        lst = lst->next;
-        i++;
-    }
-    return i;
+	int i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return i;
 }
 
 // Função para criar um array a partir da informação de line em t_env
 char **create_envpp(t_shell *shell)
 {
-    int envArraySize = ft_envsize(shell->env);
-    char **envArray = (char **)malloc((envArraySize + 1) * sizeof(char *));
-    int index = 0;
+	int envArraySize = ft_envsize(shell->env);
+	char **envArray = (char **)malloc((envArraySize + 1) * sizeof(char *));
+	int index = 0;
 
-    t_env *currentEnv = shell->env;
-    while (currentEnv != NULL)
-    {
-        if (index >= envArraySize)
-        {
-            fprintf(stderr, "Tamanho máximo do array de ambiente atingido.\n");
-            break;
-        }
+	t_env *currentEnv = shell->env;
+	while (currentEnv != NULL)
+	{
+		if (index >= envArraySize)
+		{
+			fprintf(stderr, "Tamanho máximo do array de ambiente atingido.\n");
+			break;
+		}
 
-        envArray[index] = strdup(currentEnv->line);
-        currentEnv = currentEnv->next;
-        index++;
-    }
+		envArray[index] = strdup(currentEnv->line);
+		currentEnv = currentEnv->next;
+		index++;
+	}
 
-    envArray[index] = NULL;
+	envArray[index] = NULL;
 
-    return (envArray);
+	return (envArray);
 }
 
 
@@ -68,24 +68,24 @@ int	ft_is_builtin(const char *builtin[7], char *cmd)
 
 /*-----------refatora-------------*/
 
-void handle_error(char *str)
+int handle_error(char *str)
 {
-    perror(str);
-    exit(EXIT_FAILURE);
+	perror(str);
+	return (EXIT_FAILURE);
 }
 
 void redirect_stdin(int source_fd)
 {
-    if (dup2(source_fd, STDIN_FILENO) == -1)
-        handle_error("dup2");
-    close(source_fd);
+	if (dup2(source_fd, STDIN_FILENO) == -1)
+		exit (handle_error("dup2"));
+	close(source_fd);
 }
 
 void redirect_stdout(int dest_fd)
 {
-    if (dup2(dest_fd, STDOUT_FILENO) == -1)
-        handle_error("dup2");
-    close(dest_fd);
+	if (dup2(dest_fd, STDOUT_FILENO) == -1)
+		exit (handle_error("dup2"));
+	close(dest_fd);
 }
 
 
@@ -98,9 +98,9 @@ void setup_redirection(t_pipes *pipes, int prev_pipe[2], t_shell *shell)
 	{
 		int in_fd = pipes->in;
 		if (in_fd == -1)
-			handle_error("open");
+			exit (handle_error("open"));
 		if (dup2(in_fd, STDIN_FILENO) == -1)
-			handle_error("dup2");
+			exit (handle_error("dup2"));
 		close(in_fd);
 	}
 	if (pipes->fd[OUT] != shell->std_out)
@@ -109,49 +109,52 @@ void setup_redirection(t_pipes *pipes, int prev_pipe[2], t_shell *shell)
 	{
 		out_fd = pipes->out;
 		if (out_fd == -1)
-			handle_error("open");
+			exit (handle_error("open"));
 		if (dup2(out_fd, STDOUT_FILENO) == -1)
-			handle_error("dup2");
+			exit (handle_error("dup2"));
 		close(out_fd);
 	}
 }
 
-void	ft_access(char **cmd, t_shell *shell)
+void ft_access(char **cmd, t_shell *shell)
 {
-	char	*aux;
+	int		i;
 	char	*path;
 	char	**path_array;
+	char	*full_path;
 
 	if (ft_strchr(cmd[0], '/'))
 		return ;
 	path = get_env_value(shell->env, "PATH");
 	path_array = ft_split(path, ':');
-	int i = -1;
+	i = -1;
+	full_path = NULL;
 	while (path_array[++i])
-    {
-        char *aux = cmd[0];
-        free(path);
-        path = ft_strjoin(path_array[i], "/");
-        cmd[0] = ft_strjoin(path, aux);
-        free(aux);
-        if (access(cmd[0], F_OK) == 0)
-            break;
-    }
+	{
+		free(full_path);
+		full_path = ft_strjoin(path_array[i], "/");
+		full_path = ft_strjoin(full_path, cmd[0]);
+		if (access(full_path, F_OK) == 0)
+		{
+			free(cmd[0]);
+			cmd[0] = full_path;
+			break ;
+		}
+	}
 	ft_free_array(path_array);
 	free(path);
 }
 
 void child_process(t_shell *shell, t_pipes *pipes, int prev_pipe[2])
 {
-    char **envpp;
-    envpp = create_envpp(shell);
+	char **envpp;
+	envpp = create_envpp(shell);
 
-    setup_redirection(pipes, prev_pipe, shell);
+	setup_redirection(pipes, prev_pipe, shell);
 	ft_access(&pipes->cmds[0], shell);
-	printf("executando %s\n", pipes->cmds[0]);
 	if (pipes->cmds[0][0] == '/')
 		if (execve(pipes->cmds[0], pipes->cmds, envpp) == -1)
-			handle_error("execve");
+			exit (handle_error("execve"));
 	ft_free_array(envpp);
 }
 
@@ -163,13 +166,13 @@ void execute_cmd(t_shell *shell, t_pipes *pipes, int prev_pipe[2])
 
 	pid = fork();
 	if (pid == -1)
-		handle_error("fork");
+		exit (handle_error("fork"));
 	if (pid == 0)
 		child_process(shell, pipes, prev_pipe);
 	else
 	{
 		if (waitpid(pid, &status, 0) == -1)
-			handle_error("waitpid");
+			exit (handle_error("waitpid"));
 		if (WIFEXITED(status))
 			g_exit_status = WEXITSTATUS(status);
 	}
@@ -194,7 +197,7 @@ void execute_pipeline(t_shell *shell)
 		if (pipes->next)
 		{
 			if (pipe(pipes->fd) == -1)
-				handle_error("pipe");
+				exit (handle_error("pipe"));
 		}
 		is_builtin = ft_is_builtin(builtin, pipes->cmds[0]);
 		if (is_builtin != -1)
