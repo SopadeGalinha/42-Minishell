53f6a7e02d89f9870c743f3b546bcb694514c444
Hellom Oliveira Mendes <heolivei@c1r8s17.42porto.com>
2023-11-30 22:24:12 +0000

    tira segment do export | grep A

--- srcs/execute.c
+++ srcs/execute.c
@@ -105,11 +105,7 @@ int	ft_single_cmd(t_shell *shell, t_pipes *pipes_lst)
 		if (pipes_lst->pid == -1)
 		{
 			perror("Error with creating process");
-<<<<<<< HEAD
 			return (EXIT_FAILURE);
-=======
-			return (0);
->>>>>>> 0608ff075cab82a4769cb8364012d32084bca495
 		}
 		if (pipes_lst->pid == 0)
 			ft_execve(shell, pipes_lst, shell->pipes_fd, 1, 0);
@@ -120,11 +116,7 @@ int	ft_single_cmd(t_shell *shell, t_pipes *pipes_lst)
 			g_exit_status = WEXITSTATUS(g_exit_status);
 		}
 	}
-<<<<<<< HEAD
 	return (g_exit_status);
-=======
-	return (0);
->>>>>>> 0608ff075cab82a4769cb8364012d32084bca495
 }
 
 int	ft_multiple_cmds(t_shell *shell, t_pipes *pipes_lst, int process_num, const char **builtin)
